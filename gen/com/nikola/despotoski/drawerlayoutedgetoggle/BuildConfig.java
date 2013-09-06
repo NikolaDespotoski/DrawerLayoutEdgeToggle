c621fd2c23905dc73ad8fd8c2f9f34f8ded077bc
Nikola Despotoski <nikola.despotoski@gmail.com>
2013-09-06 23:35:13 +0200

    Added NineOldAndroid library for complete backwards compat. Added ActionBarCompat support for changing title Added handle support when drawer has gravity LEFT or END

---
+++ gen/com/nikola/despotoski/drawerlayoutedgetoggle/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.nikola.despotoski.drawerlayoutedgetoggle;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- src/com/nikola/despotoski/drawerlayoutedgetoggle/DrawerLayoutEdgeToggle.java
+++ src/com/nikola/despotoski/drawerlayoutedgetoggle/DrawerLayoutEdgeToggle.java
@@ -1,14 +1,16 @@
 
-package com.example.android.navigationdrawerexample;
+package com.nikola.despotoski.drawerlayoutedgetoggle;
 
 import android.app.Activity;
 import android.content.res.Configuration;
+import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.ViewDragHelper;
-import android.util.Log;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Display;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -16,14 +18,13 @@ import android.view.View;
 import android.view.View.MeasureSpec;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.TranslateAnimation;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.FrameLayout.LayoutParams;
 import android.widget.ImageView;
+
+import com.nineoldandroids.view.ViewHelper;
+import com.nineoldandroids.view.ViewPropertyAnimator;
 /* Author: Nikola Despotoski
  * Email: nikola[dot]despotoski(at)gmail[dot]com
  * 
@@ -41,7 +42,7 @@ import android.widget.ImageView;
    See the License for the specific language governing permissions and
    limitations under the License.
  */
-public class DrawerLayoutEdgeToggle implements DrawerLayout.DrawerListener, AnimationListener{
+public class DrawerLayoutEdgeToggle implements DrawerLayout.DrawerListener{
 
 	private Activity mActivity;
 	private DrawerLayout mDrawerLayout;
@@ -58,10 +59,10 @@ public class DrawerLayoutEdgeToggle implements DrawerLayout.DrawerListener, Anim
 		@Override
 		public void onClick(View v) {
 			if(mDefaultOk){
-				if(!mDrawerLayout.isDrawerOpen(GravityCompat.START))
-					mDrawerLayout.openDrawer(GravityCompat.START);
+				if(!mDrawerLayout.isDrawerOpen(mGravity))
+					mDrawerLayout.openDrawer(mGravity);
 				else
-					mDrawerLayout.closeDrawer(GravityCompat.START);
+					mDrawerLayout.closeDrawer(mGravity);
 			}
 			if(mHandleClickListener != null)
 				mHandleClickListener.onClick(v);
@@ -91,6 +92,7 @@ public class DrawerLayoutEdgeToggle implements DrawerLayout.DrawerListener, Anim
 	private int mCloseTitleRes = 0x0;
 	private int mOpenTitleRes =  0x0;
 	private OnTouchListener mHandleTouchListener;
+	protected int mGravity;
 	
 	private View.OnAttachStateChangeListener mOnAttachListener = new View.OnAttachStateChangeListener() {
 		
@@ -107,16 +109,22 @@ public class DrawerLayoutEdgeToggle implements DrawerLayout.DrawerListener, Anim
 			 }
 		}
 	};
+	private FrameLayout rootLayout;
+	private Drawable mCurrentDrawable;
+	private int mScreenWidth;
 	
- public DrawerLayoutEdgeToggle(Activity a, DrawerLayout l, int drawerOpen, int drawerClose, int gravity, boolean keepShadowOnHandle){
-		if(gravity == Gravity.CENTER || gravity == Gravity.CENTER_HORIZONTAL || gravity == Gravity.CENTER_HORIZONTAL ){
+ public DrawerLayoutEdgeToggle(Activity a, DrawerLayout l, int drawerOpen, int drawerClose, int handleGravity, boolean keepShadowOnHandle, int drawerGravity){
+		if(handleGravity == Gravity.CENTER || handleGravity == Gravity.CENTER_HORIZONTAL || handleGravity == Gravity.CENTER_HORIZONTAL ){
 			throw new IllegalArgumentException("Don't use any CENTER gravity");
 		}
+		if(drawerGravity != GravityCompat.END && drawerGravity != GravityCompat.START && drawerGravity != Gravity.LEFT && drawerGravity != Gravity.RIGHT )
+			throw new IllegalArgumentException("Use: GravityCompat.END, GravityCompat.START, Gravity.LEFT or Gravity.RIGHT for drawerGravity parameter");
+		mGravity = drawerGravity;
 		mActivity = a;
 		mDrawerLayout = l;
 		mOpenDrawable = a.getResources().getDrawable(drawerOpen);
 	    mCloseDrawable = a.getResources().getDrawable(drawerClose);
-	    FrameLayout rootLayout = (FrameLayout)mActivity.findViewById(android.R.id.content);
+	    rootLayout = (FrameLayout)mActivity.findViewById(android.R.id.content);
 	    if(Build.VERSION.SDK_INT <= 12){
 			mHandle = new ImageView(a){
 	
@@ -129,17 +137,26 @@ public class DrawerLayoutEdgeToggle implements DrawerLayout.DrawerListener, Anim
 			};
 	    }else{
 	    	mHandle = new ImageView(a);
+	    	mDrawerLayout.measure(MeasureSpec.EXACTLY, MeasureSpec.EXACTLY);
+	    	
 	    }
 		mHandle.setOnClickListener(mHandleClickListenerInt);	
 		mHandle.setOnTouchListener(mHandleTouchListenerInt);
 		mHandle.setSaveEnabled(true);
-		rootLayout.addView(mHandle, new FrameLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, gravity));
+		rootLayout.addView(mHandle, new FrameLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, handleGravity));
 		mKeepShadowOnHandle = keepShadowOnHandle;
-		if(Build.VERSION.SDK_INT >= 12)
+		mCurrentDrawable = mCloseDrawable;
+		mScreenWidth = getScreenWidth();
+		if(Build.VERSION.SDK_INT >= 12){
 			mDrawerLayout.addOnAttachStateChangeListener(mOnAttachListener);
+			
+		}
+		
+		
 		
 	
 	}
+ 
 	public void setOverrideDefaultHandleAction(boolean ok){
 		mDefaultOk  = ok;
 	}
@@ -157,13 +174,14 @@ public class DrawerLayoutEdgeToggle implements DrawerLayout.DrawerListener, Anim
 	public void onDrawerClosed(View arg0) {
 		mHandle.setImageDrawable(mCloseDrawable);
 		updateActionBar();
-		
+		mCurrentDrawable = mCloseDrawable;
 	}
 
 	@Override
 	public void onDrawerOpened(View arg0) {
 		mHandle.setImageDrawable(mOpenDrawable);	
 		updateActionBar();		
+		mCurrentDrawable = mOpenDrawable;
 	}
 	public void setActionBarCloseTitle(int res){
 		mCloseTitleRes = res;
@@ -175,15 +193,13 @@ public class DrawerLayoutEdgeToggle implements DrawerLayout.DrawerListener, Anim
 	@Override
 	public void onDrawerSlide(View arg0, float slideOffset) {	
 		getDrawerMinusShadow();
-		  float translationX = slideOffset*mMinusShadow;
+		  float translationX = checkForLeftDrawer(mGravity,slideOffset*mMinusShadow);
+		 // Log.i("translationX ", " "+translationX);
 		if(Build.VERSION.SDK_INT >= 11){
 			mHandle.setTranslationX(translationX);
 			mHandle.setX(mHandle.getTranslationX());
 		}else{
-			HandleTranslateAnimation translateAnimation = new HandleTranslateAnimation(0, translationX, 0,0);
-			translateAnimation.setDuration(0);
-			translateAnimation.setAnimationListener(this);
-			mHandle.startAnimation(translateAnimation);
+			ViewPropertyAnimator.animate(mHandle).translationX(translationX).setDuration(0).start();
 		}
 		if(!mKeepShadowOnHandle)
 			 noShadow();
@@ -196,23 +212,31 @@ public class DrawerLayoutEdgeToggle implements DrawerLayout.DrawerListener, Anim
 	public void onDrawerStateChanged(int arg0) {
 	
 	}
+	private float checkForLeftDrawer(int gravity, float x){
+		return gravity == GravityCompat.END ||  gravity == Gravity.LEFT? mScreenWidth-x-mHandle.getDrawable().getIntrinsicWidth() : x;
+	}
+
 	private void getDrawerMinusShadow(){
 		if(mMinusShadow == 0.0f){
 			for(int i = 0 ; i < mDrawerLayout.getChildCount();i++){
 				mMinusShadow = mMinusShadow == 0.0f ? mDrawerLayout.getChildAt(i).getMeasuredWidth() : mMinusShadow;
-				mMinusShadow = Math.min(mMinusShadow, mDrawerLayout.getChildAt(i).getMeasuredWidth());
-				
-			}
-			
+				mMinusShadow = Math.min(mMinusShadow, mDrawerLayout.getChildAt(i).getMeasuredWidth());	
+		
+			}			
 		}
 		return;
 	}
 	private void keepShadow(float offset) {
-		mHandle.setAlpha(MAX_VIEW_ALPHA - offset);
+		if(Build.VERSION.SDK_INT >= 11)
+			mHandle.setAlpha(MAX_VIEW_ALPHA - offset);
+		else
+			mHandle.getDrawable().setAlpha((int) (MAX_ALPHA*(1.0f - offset)));
 	}
 
 	public void syncState(){
-			mHandle.setImageDrawable(mDrawerLayout.isDrawerOpen(Gravity.START)? mOpenDrawable : mCloseDrawable);
+			mCurrentDrawable = mDrawerLayout.isDrawerOpen(mGravity)? mOpenDrawable : mCloseDrawable;
+			mScreenWidth = getScreenWidth();
+			mHandle.setImageDrawable(mCurrentDrawable);
 			updateActionBar();
 			updateHandle();
 			
@@ -223,65 +247,63 @@ public class DrawerLayoutEdgeToggle implements DrawerLayout.DrawerListener, Anim
 	}
 	private void updateHandle(){
 		getDrawerMinusShadow();
-		Log.i("Minus Shadow: "," "+ mMinusShadow);
+		mScreenWidth = getScreenWidth();
+		//Log.i("Minus Shadow: "+mHandle.getDrawable().getIntrinsicWidth()," Shadow: "+ checkForLeftDrawer(mGravity,mMinusShadow) + " Screen w: "+ mScreenWidth + " Difference: "+(mScreenWidth-mMinusShadow));
 		if(isOpen()){
-			mHandle.setX(mMinusShadow);
+			//Log.i("isOpen()"+mGravity," X: "+checkForLeftDrawer(mGravity,mMinusShadow));
+			if(Build.VERSION.SDK_INT >= 11)
+				mHandle.setTranslationX(checkForLeftDrawer(mGravity,mMinusShadow));
+			else
+				mHandle.setImageDrawable(mCurrentDrawable);
+				ViewHelper.setX(mHandle, checkForLeftDrawer(mGravity,mMinusShadow));
+			
 		}
 	}
-	
+	@SuppressWarnings("deprecation")
+	private int getScreenWidth(){
+		
+		  Point size = new Point();
+		  WindowManager w = mActivity.getWindowManager();
+		  if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2){
+	          w.getDefaultDisplay().getSize(size);
+	          return size.x; 
+	        }else{
+	          Display d = w.getDefaultDisplay(); 
+	          return d.getWidth(); 
+		 }
+	}
+
 	private void updateActionBar(){
 		if(mOpenTitleRes != 0x0 && mCloseTitleRes != 0x0)
 			if(mOpenTitleRes != 0x0 && mCloseTitleRes != 0x0)
-			    mActivity.getActionBar().setTitle(isOpen()? mActivity.getResources().getString(mOpenTitleRes) : mActivity.getResources().getString(mCloseTitleRes));
+			{
+				String title = isOpen()? mActivity.getResources().getString(mOpenTitleRes) : mActivity.getResources().getString(mCloseTitleRes);
+				if(Build.VERSION.SDK_INT >= 11)
+					mActivity.getActionBar().setTitle(title);
+				else if(mActivity instanceof ActionBarActivity){
+					((ActionBarActivity)mActivity).getSupportActionBar().setTitle(title);
+				}
+			}
 	}
 	private boolean isOpen(){
-		return mDrawerLayout.isDrawerOpen(GravityCompat.START);
+		return mDrawerLayout.isDrawerOpen(mGravity);
 	}
 	public boolean onOptionsItemSelected(MenuItem item) {
 		if(isOpen()){
-			mDrawerLayout.closeDrawer(GravityCompat.START);
+			mDrawerLayout.closeDrawer(mGravity);
 		}
 		return false;
 	}
+//	public void setVerticalTopOffset(int topInPercentage){
+//		if(topInPercentage > 100 || topInPercentage < 0){
+//			throw new IllegalArgumentException("Invalid percentage.");
+//		}
+//	    int actionBarHeight = mActivity instanceof ActionBarActivity? ((ActionBarActivity)mActivity).getSupportActionBar().getHeight() : mActivity.getActionBar().getHeight();
+//	    
+//	}
 	public void onConfigurationChanged(Configuration newConfig) {
 		
 	}
-	@Override
-	public void onAnimationEnd(Animation animation) {
-		 HandleTranslateAnimation hand = (HandleTranslateAnimation)animation;
-		 mHandle.setX(hand.getmToXDelta());
-	}
-	@Override
-	public void onAnimationRepeat(Animation animation) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void onAnimationStart(Animation animation) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	private class HandleTranslateAnimation extends TranslateAnimation{
 
-		private float mFromXDelta=0.0f;
-		private float mToXDelta= 0.0f;
-
-		public float getmFromXDelta() {
-			return mFromXDelta;
-		}
-
-		public float getmToXDelta() {
-			return mToXDelta;
-		}
-
-		public HandleTranslateAnimation(float fromXDelta, float toXDelta,
-				float fromYDelta, float toYDelta) {
-			super(fromXDelta, toXDelta, fromYDelta, toYDelta);
-			this.mFromXDelta = fromXDelta; 
-			this.mToXDelta  = toXDelta; 
-		}
-		
-	}
 	
 }
